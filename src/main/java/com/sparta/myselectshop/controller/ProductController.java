bb35945c1d90f062db1326b5918f7a3e0fad9622
hellonaeunkim <hellonaeunkim@gmail.com>
2025-09-24 15:40:58 +0900

    [FEAT] 관심 상품에 폴더 추가 및 폴더 전체 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -45,4 +45,13 @@ public class ProductController {
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return productService.getProducts(userDetails.getUser(), page - 1, size, sortBy, isAsc);
     }
+
+    @PostMapping("/products/{productId}/folders")
+    public void addFolders(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
 }
